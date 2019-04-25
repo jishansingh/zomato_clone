6d3a7b624579f2e3ecf73afa80d78cebe861d96a
jishansingh <singhjishan2@gmail.com>
2019-04-25 17:14:22 +0545

    some progress

--- products/forms.py
+++ products/forms.py
@@ -38,6 +38,8 @@ class CustomerForm(forms.ModelForm):
         fields=('image','city','state','about','location')
 
 class AddMenu(forms.ModelForm):
+    name=forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    description=forms.CharField(widget=forms.Textarea(attrs={'class' : 'form-control',}))
     class Meta:
         model=Menu
         fields=('name','image','description','price','veg')
--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index,name='index'),
     path('send/', views.send_request,name='send_request'),
+    path('edit_menu/', views.edit_menu,name='edit_menu'),
     path('edit/', views.edit_profile,name='edit_profile'),#list_restraunt
     path('list/<id>/', views.list_restraunt,name='list_restraunt'),
     path('add/',views.add_restraunt,name='add_restraunt'),
